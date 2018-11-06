6232a9e28446d94ca0f14bf74b1d394fe18f2540
JayZang <sa841218as@gmail.com>
2018-11-06 16:36:33 +0800

    處理多個cookies

---
+++ 07_cookie/02_multi-cookies/main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", set)
+	http.HandleFunc("/read", read)
+	http.HandleFunc("/more", more)
+
+	http.ListenAndServe(":8080", nil)
+}
+
+func set(res http.ResponseWriter, req *http.Request) {
+	http.SetCookie(res, &http.Cookie{
+		Name:  "my-cookie",
+		Value: "stuff",
+	})
+
+	fmt.Fprintln(res, "COOKIE WRITTEN - CHECK YOUR BROWSER")
+	fmt.Fprintln(res, "in chrome go to: dev tools / application/ cookies")
+}
+
+func more(res http.ResponseWriter, req *http.Request) {
+	http.SetCookie(res, &http.Cookie{
+		Name:  "general",
+		Value: "THIS GENERAL",
+	})
+
+	http.SetCookie(res, &http.Cookie{
+		Name:  "more",
+		Value: "THIS MORE",
+	})
+
+	fmt.Fprintln(res, "COOKIE WRITTEN - CHECK YOUR BROWSER")
+	fmt.Fprintln(res, "in chrome go to: dev tools / application/ cookies")
+}
+
+func read(res http.ResponseWriter, req *http.Request) {
+	c1, err := req.Cookie("my-cookie")
+	if err != nil {
+		log.Println(err)
+	} else {
+		fmt.Fprintln(res, "YOUR COOKIE:", c1)
+	}
+
+	c2, err := req.Cookie("general")
+	if err != nil {
+		log.Println(err)
+	} else {
+		fmt.Fprintln(res, "YOUR COOKIE:", c2)
+	}
+
+	c3, err := req.Cookie("more")
+	if err != nil {
+		log.Println(err)
+	} else {
+		fmt.Fprintln(res, "YOUR COOKIE:", c3)
+	}
+}
