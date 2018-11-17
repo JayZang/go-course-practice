7ed4c877c12193f08c6e149cb1f861b3db2143a8
JayZang <sa841218as@gmail.com>
2018-11-17 12:03:52 +0800

    查看 context 內容

---
+++ 11_context/01_what-is-it/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", index)
+	http.ListenAndServe(":8080", nil)
+}
+
+func index(w http.ResponseWriter, req *http.Request) {
+	ctx := req.Context()
+	fmt.Fprintln(w, ctx)
+}
